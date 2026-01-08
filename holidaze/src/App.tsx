04e39dd8138a704a4b0de5ab5a73101ce3a27526
SocanIcode <azebsame@gmail.com>
2026-01-09 00:11:20 +0100

    Setup Vite React TS + Tailwind

---
+++ holidaze/src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold">Holidaze</h1>
+    </div>
+  );
+}
